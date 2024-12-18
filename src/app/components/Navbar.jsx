c528fdd90bc12f2cf4f4c829fc8bdd7e229d21a2
D4rthSi <narenprax@ucla.edu>
2024-12-17 23:03:08 -0600

    Working on mobile menu

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Link from "next/link"
 import next from 'next'
 import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
 
 const navLinks = [
     {
@@ -23,12 +25,27 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+    const { navbarOpen, setNavbarOpen } = useState(false);
+
     return (
-        <nav>
-            <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+        <nav className='fixed top-0 left-0 right-0 z-10, bg-opacity-90'>
+            <div className='flex flex-wrap items-center justify-between mx-auto p-8 px-4 py-2'>
                 <Link href="/" className='text-5xl text-white font-semibold'>LOGO</Link>
+                <div className='mobile-menu block md:hidden'>
+                    {
+                        !navbarOpen ? (
+                            <button onClick={() => setNavbarOpen(true)} className='flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white'>
+                                <Bars3Icon className='h-5 w-5' />
+                            </button>
+                        ) : (
+                                <button onClick={() => setNavbarOpen(false)} className='flex items-center px-3 py-2 border rounded text-slate-200 hover:text-white'>
+                                    <XMarkIcon className='h-5 w-5' />
+                            </button>
+                        )
+                    }
+                </div>
                 <div className='menu hidden md:block md:w-auto'>
-                    <ul>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8'>
                         {
                             navLinks.map((link, index) => (
                                 <li key={index}>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
@@ -6,7 +5,7 @@ export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-black">
         <Navbar />
-        <div className = "container mx-auto px-12 py-4">
+        <div className = "container mt-24 mx-auto px-12 py-4">
           <HeroSection />
         </div>
         

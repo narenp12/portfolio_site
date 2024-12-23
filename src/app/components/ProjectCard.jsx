0b217b1a01a28b894736738e49875c55a12dc510
D4rthSi <narenprax@ucla.edu>
2024-12-23 00:19:15 -0600

    Fixed project layout, now need to make page structure for writeups

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/solid'
-import Link from 'next/link'
+import React from 'react';
+import { CodeBracketIcon, EyeIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
 
-const ProjectCard = (imgURL, title, description) => {
+const ProjectCard = ({ imgURL, title, description }) => {
     return (
-        <div>
-            <div className='h-52 md:h-72 rounded-t-xl relative group' style={{ background: 'url(${imgURL})', backgroundSize: "cover" }}>
-                <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-300'>
-                    <Link href="/" className='mr-2 h-14 w-14 border-2 relative rounded-full border-gray-200 hover:border-white'>
-                    <CodeBracketIcon className='h-10 w-10 text-gray-200 cursor-pointer absolute hover:text-white top-1/2 left-1/2 transform translate-x-1/2 translate-y-1/2'/>
-                    </Link>
-                    <Link href="/" className='h-14 w-14 border-2 relative rounded-full border-gray-200 hover:border-white'>
-                        <EyeIcon className='h-10 w-10 text-gray-200 cursor-pointer absolute hover:text-white top-1/2 left-1/2 transform translate-x-1/2 translate-y-1/2' />
+        <div className="bg-gray-800 rounded-xl overflow-hidden shadow-md">
+            {/* Background Image */}
+            <div
+                className="h-52 md:h-72 relative group"
+                style={{
+                    backgroundImage: `url(${imgURL})`,
+                    backgroundSize: 'cover',
+                }}
+            >
+                {/* Overlay with Icons */}
+                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-80 transition-all duration-300">
+                    <Link
+                        href="/"
+                        className="mr-2 h-14 w-14 border-2 rounded-full border-gray-200 hover:border-white flex items-center justify-center"
+                    >
+                        <CodeBracketIcon className="h-10 w-10 text-gray-200 hover:text-white" />
                     </Link>
                 </div>
-
             </div>
-            <div className='text-white rounded-b-xl py-6 px-4'>
-                <h5 className='font-xl font-semibold mb-2'>{title}</h5>
-                <p className='text-gray-200'>description</p>
+
+            {/* Title and Description */}
+            <div className="bg-gray-900 text-white p-4">
+                <h5 className="text-xl font-semibold mb-2">{title || 'Untitled Project'}</h5>
+                <p className="text-gray-400">{description || 'No description available.'}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectCard
+export default ProjectCard;

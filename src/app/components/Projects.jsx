474b867284cc93cc0caeda3ac28326dd0ee3eeaf
D4rthSi <narenprax@ucla.edu>
2024-12-26 23:07:50 -0600

    Tried to add project tags but failed, commented for now will fix later

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag';
 
 const projectsData = [
     {
@@ -19,11 +21,29 @@ const projectsData = [
 ];
 
 const Projects = () => {
+    const [tag, setTag] = useState("All")
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) => {
+        project.tag.includes(tag)
+    })
+
     return (
         <>
             <h2 className='text-center text-4xl font-bold text-white mt-4 mb-4' id="projects">
                 My Projects
             </h2>
+            { /*
+                            <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
+                <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} /> 
+                <ProjectTag onClick={handleTagChange} name="Python" isSelected={tag === "Python"} />
+            </div>
+
+            */ }
+
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {projectsData.map((project) => (
                     <ProjectCard
@@ -31,7 +51,7 @@ const Projects = () => {
                         title={project.title}
                         description={project.description}
                         imgURL={project.image}
-                        id = {project.id}
+                        id={project.id}
                     />
                 ))}
             </div>

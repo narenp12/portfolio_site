e7e4fbd5e3d0e78a6c8190deac80964e1c8a5aab
D4rthSi <narenprax@ucla.edu>
2025-01-04 23:54:05 -0800

    Added repo link to blog post - why I made the website

--- src/app/blog2/page.jsx
+++ src/app/blog2/page.jsx
@@ -20,7 +20,7 @@ export default function Post1() {
 
             {/* Project Tagline */}
             <p className="text-xl text-white mb-8 text-center">
-               My own little place to post into the void
+                My own little place to post into the void
             </p>
 
             {/* Project Image */}
@@ -36,21 +36,21 @@ export default function Post1() {
             {/* Post */}
             <section className="mb-12 w-full max-w-4xl mx-auto font-sans">
                 <p className="text-lg mb-6">
-                    I can't tell you how many times I've looked up cool people on Google and they've had their own little nice website with fancy logos and nice looking pages and all that. 
-                    To be frank, that was probably the main reason I wanted my own website. 
+                    I can't tell you how many times I've looked up cool people on Google and they've had their own little nice website with fancy logos and nice looking pages and all that.
+                    To be frank, that was probably the main reason I wanted my own website.
                     I too wanted a shiny place that was all about me.
                 </p>
 
                 <p className="text-lg mb-6">
-                    Despite having this in mind for quite a while, I never actually tried to make one of my own until recently. 
-                    For the longest time I had assumed even getting a domain name would mean shelling out money for eternity just to have a site I could send to people. 
+                    Despite having this in mind for quite a while, I never actually tried to make one of my own until recently.
+                    For the longest time I had assumed even getting a domain name would mean shelling out money for eternity just to have a site I could send to people.
                     Who knew how many people would actually look at it, let alone view the other content?
                 </p>
 
                 <p className="text-lg mb-6">
-                    Ultimately, what pushed me to really try it out was wanting to just have one place where I could show all my friends and family the little things I did in my free time that I thought were cool. 
+                    Ultimately, what pushed me to really try it out was wanting to just have one place where I could show all my friends and family the little things I did in my free time that I thought were cool.
                     At the end of the day, even if no one else viewed this website organically, I would be happy just having a place with a nice little write up for everything I've tinkered with.
-                    The sudden motivation for this website really came in late September 2024 on a random weekend. That's what led me to creating my first website using netlify. 
+                    The sudden motivation for this website really came in late September 2024 on a random weekend. That's what led me to creating my first website using netlify.
                     I didn't exactly know how to do web development with code directly, so I just used their visual editor and a website template and called it a day.
                     One night later, I had a fully functional website with posts I could show everyone. However, the longer I had the site, the more discontent I became with it. I almost felt like I cheated to get a website that wasn't even cohesive and didn't represent what I wanted my website to say about me.
                     With a busy school quarter, I pushed it down the line with the intention of eventually creating a site I was proud of.
@@ -63,14 +63,21 @@ export default function Post1() {
                 </p>
 
                 <p className="text-lg mb-6">
-                    Is my website amazing and impressive? I don't know, but I'm glad I have my own little place to put things that I want to see. The experience of creating the site was a blast, and I hope to mess around with a lot more in the future, so I can add even more to this website. 
+                    Is my website amazing and impressive? I don't know, but I'm glad I have my own little place to put things that I want to see. The experience of creating the site was a blast, and I hope to mess around with a lot more in the future, so I can add even more to this website.
                     If anyone stumbles across this in the future and would like to give me feedback on my website (or even my code) I would greatly appreciate it. This is something I hope to continue working on for the rest of my life and I hope 2025 brings more things for me to be proud of. Here's to the future.
                 </p>
 
                 <p className="text-base mb-6">
                     Thank you for reading my rambling justification for the existence of this website. If you think I could make this better (I definitely can) I would love to hear new ideas.
                 </p>
-                
+
+                <p>
+                    You can check out the code I used to create it <Link
+                        href="https://github.com/narenp12/portfolio_site"
+                        className="text-white underline"
+                    > here! </Link>
+                </p>
+
             </section>
         </main>
     );

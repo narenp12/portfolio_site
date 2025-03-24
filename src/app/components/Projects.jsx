2224a27e12d2750aec48e10086ab8db315b33011
narenp12 <narenprax@ucla.edu>
2025-03-24 01:04:29 -0500

    Changed filtered projects as output

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -42,7 +42,7 @@ const Projects = () => {
             </div>
 
             <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-                {projectsData.map((project) => (
+                {filteredProjects.map((project) => (
                     <ProjectCard
                         key={project.id}
                         title={project.title}

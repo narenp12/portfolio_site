32af0928f7f8512bd6d66aa5378eca3c7936e41a
narenp12 <narenprax@ucla.edu>
2025-04-02 23:05:26 -0700

    Test redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: "/#blog",
+          destination: "https://narenblog.netlify.app",
+          permanent: true,
+        },
+      ];
+    },
+  };  
+
 export default nextConfig;
